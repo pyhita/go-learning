6facf68bd36606cff260b92e62fd86307dc704bd
kante.yang <kante.yang@shopee.com>
2024-09-23 14:31:37 +0800

    feat: go slice补充

--- basic_go/syntax/builtin_type/main.go
+++ basic_go/syntax/builtin_type/main.go
@@ -2,5 +2,9 @@ package main
 
 func main() {
 	//TestSlice1()
-	TestSlice2()
+	//TestSlice2()
+
+	//TestSlice3()
+
+	TestSlice4()
 }
--- basic_go/syntax/builtin_type/slice.go
+++ basic_go/syntax/builtin_type/slice.go
@@ -47,3 +47,27 @@ func TestSlice2() {
 		fmt.Println("equals")
 	}
 }
+
+// 测试切片的截取操作，左闭右开
+func TestSlice3() {
+	s1 := []int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("s1: %v, len %d, cap %d \n", s1, len(s1), cap(s1))
+
+	s2 := s1[0:4]
+	fmt.Printf("s2: %v, len %d, cap %d \n", s2, len(s2), cap(s2))
+
+	s3 := s1[1:]
+	fmt.Printf("s3: %v, len %d, cap %d \n", s3, len(s3), cap(s3))
+
+	s4 := s1[:4]
+	fmt.Printf("s4: %v, len %d, cap %d \n", s4, len(s4), cap(s4))
+}
+
+// 切片的判空 操作
+func TestSlice4() {
+	var s1 []int
+	fmt.Println("s1 is nil ", s1 == nil)
+
+	var s2 = []int{}
+	fmt.Println("s2 is nil ", s2 == nil)
+}
