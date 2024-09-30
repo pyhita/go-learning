2849fa00a2aa77e9d1eb141684957916be98592b
kante.yang <kante.yang@shopee.com>
2024-09-30 14:30:39 +0800

    feat: gin example

--- go-web/gin/gin-quickstart/server.go
+++ go-web/gin/gin-quickstart/server.go
@@ -1,10 +1,50 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"time"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+
+		// Set example variable
+		c.Set("example", "12345")
+
+		// before request
+
+		c.Next()
+
+		// after request
+		latency := time.Since(t)
+		log.Print(latency)
+
+		// access the status we are sending
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
 
 func main() {
 	// 创建一个默认的路由引擎
 	r := gin.Default()
+
+	// add middleware
+	r.Use(func(c *gin.Context) {
+		println("this is the first middleware")
+	}, func(c *gin.Context) {
+		println("this is the second middleware")
+	})
+	r.Use(Logger())
+
+	// test custom middleware
+	r.GET("/middleware", func(c *gin.Context) {
+		str := c.MustGet("example").(string)
+		log.Println(str)
+	})
 
 	r.GET("/book", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -37,6 +77,20 @@ func main() {
 		context.JSON(200, gin.H{
 			"result": "success",
 		})
+	})
+
+	// gin 参数路由
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.Writer.WriteString("username is " + name)
+	})
+
+	// gin 查询参数
+	r.GET("/welcome", func(c *gin.Context) {
+		firstname := c.Query("firstname")
+		lastname := c.DefaultQuery("lastname", "Gopher")
+
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
 	})
 
 	// 启动HTTP服务，默认在0.0.0.0:8080启动服务
