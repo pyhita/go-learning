520635a73fe41e4433b80e40046f9207997491d3
kante.yang <kante.yang@shopee.com>
2024-11-11 11:02:34 +0800

    test rebase 1

---
+++ basic_go/syntax/goroutine/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	c := foo(func() error {
+		time.Sleep(2 * time.Second)
+		return errors.New("timeout")
+	})
+
+	fmt.Println(<-c, "goroutine exit.")
+}
+
+func foo(f func() error) chan error {
+	c := make(chan error)
+	go func() {
+		c <- f()
+	}()
+
+	return c
+}
